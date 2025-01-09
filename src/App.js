58b4d67d1c556263b738177aec933ec715138cee
MohammedMuneeb <mohammedmuneeb33@gmail.com>
2025-01-09 13:11:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React ,{useState ,useEffect }from 'react'; 
+import axios from 'axios'; 
+
+const Search = () => {
+  const [query, setQuery] = useState('');
+  const [videos, setVideos] = useState([]);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  }; 
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try { 
+      const response =await axios.get (`https://www.googleapis.com/youtube/v3/search`, {
+        params: {
+          part: 'snippet',
+          maxResults: 10,
+          q: query,
+          key: process.env.REACT_APP_YOUTUBE_API_KEY , 
+        }, 
+      }); 
+      setVideos (response.data.items ); 
+    }catch (error ){ 
+      console.error('Error fetching data:', error ); 
+      // Handle errors appropriately (e.g., display an error message)
+    } 
+  }; 
+  return ( 
+    <div > 
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          value={query} 
+          onChange={handleInputChange} 
+          placeholder="Search YouTube" 
+        /> 
+        <button type="submit">Search</button>
+      </form > 
+      <ul > 
+        {videos.map ((video )=>( 
+          <li key={video.id.videoId}>
+            <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">
+              {video.snippet.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ); 
+}; 
+export default Search ; 
